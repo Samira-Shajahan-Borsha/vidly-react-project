bac9debf0f938d56c4dea139d9026bb1d4b9becc
Samira Shajahan Borsha <samiraborsha08@gmail.com>
2022-09-08 18:33:21 +0600

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,24 @@
-import './App.css';
-import React, { Component } from 'react';
-import Movies from './Component/movies';
-
+import "./App.css";
+import React, { Component } from "react";
+import { Route, Redirect, Switch } from "react-router-dom";
+import Movies from "./Component/movies";
+import NotFound from "./Component/notFound";
+import Customers from "./Component/customers";
+import Rentals from "./Component/rentals";
 
 class App extends Component {
-  render(){
+  render() {
     return (
-        <main className='container'>
-            <Movies/>
-        </main>
+      <main className="container">
+        <Switch>
+          <Route path="/movies" component={Movies} />
+          <Route path="/customers" component={Customers} />
+          <Route path="/rentals" component={Rentals} />
+          <Route path="/not-found" component={NotFound} />
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
+      </main>
     );
   }
 }
