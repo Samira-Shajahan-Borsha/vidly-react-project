d7e9549384ca642bc3f24fc3d6a2657465ff1ede
Samira Shajahan Borsha <samiraborsha08@gmail.com>
2022-09-13 18:38:34 +0600

    searchBox added

--- src/Component/movies.jsx
+++ src/Component/movies.jsx
@@ -5,7 +5,9 @@ import { getMovies } from "../services/fakeMovieService";
 import { getGenres } from "../services/fakeGenreService";
 import { paginate } from "../utils/paginate";
 import MoviesTable from "./moviesTable";
+import SearchBox from "./searchBox";
 import _ from "lodash";
+import { Link } from "react-router-dom";
 
 class Movies extends Component {
   state = {
@@ -13,6 +15,8 @@ class Movies extends Component {
     genres: [],
     currentPage: 1,
     pageSize: 4,
+    searchQuery: "",
+    selectedGenre: null,
     sortColumn: { path: "title", order: "asc" },
   };
 
@@ -42,7 +46,11 @@ class Movies extends Component {
   };
 
   handleGenreSelect = (genre) => {
-    this.setState({ selectedGenre: genre, currentPage: 1 });
+    this.setState({ selectedGenre: genre, searchQuery: "", currentPage: 1 });
+  };
+
+  handleSearch = (query) => {
+    this.setState({ searchQuery: query, selectedGenre: null, currentPage: 1 });
   };
 
   handleSort = (sortColumn) => {
@@ -55,13 +63,21 @@ class Movies extends Component {
       pageSize,
       sortColumn,
       selectedGenre,
+      searchQuery,
       movies: allMovies,
     } = this.state;
 
-    const filtered =
-      selectedGenre && selectedGenre._id
-        ? allMovies.filter((m) => m.genre._id === selectedGenre._id)
-        : allMovies;
+    let filtered = allMovies;
+
+    if (searchQuery)
+      filtered = allMovies.filter((m) =>
+        m.title.toLowerCase().startsWith(searchQuery.toLowerCase())
+      );
+
+    
+    else if(selectedGenre && selectedGenre._id) 
+        filtered = allMovies.filter((m) => m.genre._id === selectedGenre._id);
+        
 
     const sorted = _.orderBy(filtered, [sortColumn.path], [sortColumn.order]);
 
@@ -71,7 +87,6 @@ class Movies extends Component {
   };
 
   render() {
-    
     const { length: count } = this.state.movies;
     const { currentPage, pageSize, sortColumn } = this.state;
 
@@ -90,7 +105,17 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
+          <Link
+            to="/movies/new"
+            className="btn btn-primary"
+            style={{ marginTop: 50 }}
+          >
+            New Movie
+          </Link>
           <p className="m-5">Showing {totalCount} movies in the database</p>
+
+          <SearchBox value={this.searchQuery} onChange={this.handleSearch} />
+
           <MoviesTable
             movies={movies}
             sortColumn={sortColumn}
---
+++ src/Component/searchBox.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Input from "./common/input";
+const SearchBox = ({value,onChange}) => {
+  return (
+    <Input
+      type="text"
+      name="query"
+      classname="form-control my-3"
+      placeholder="Search..."
+      value={value}
+      onChange={e => onChange(e.currentTarget.value)}
+    />
+  );
+};
+
+export default SearchBox;
